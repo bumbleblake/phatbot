478fe2fd6b34c5326e44274b2a735abdf1de5e06
Blake Bain <bumbleblake@users.noreply.github.com>
2018-01-21 11:20:23 +1300

    Update index.js

--- index.js
+++ index.js
@@ -352,7 +352,7 @@ client.on("message", async message => {
                else if(args.join(' ').endsWith("?")){
                 var ques = (args.join(" "));
                }
-                if(ques.split('').length > 33) return message.reply(`would you please try asking a question that isn't ${ques.split('').length} characters long`);
+                if(ques.split('').length > 66) return message.reply(`would you please try asking a question that isn't ${ques.split('').length} characters long`);
                message.channel.sendEmbed({
                    color: 0xffe23c,
                    author: {
@@ -388,7 +388,7 @@ client.on("message", async message => {
             }
                 var thing1 = args[0].toString();
                 var thing2 = args[1].toString();
-                if(thing1.split('').length > 33 || thing2.split('').length > 33) return message.reply("OI MATE WOULD YOU MIND PICKING A SHORTER NAME");
+                if(thing1.split('').length > 50 || thing2.split('').length > 50) return message.reply("OI MATE WOULD YOU MIND PICKING A SHORTER NAME");
                 var matches = ["shadow teddy", "shadow teddy1424", "teddy shadow", "teddy1424 shadow", "happy teddy", "happy teddy1424", "teddy happy", "teddy1424 happy", "victoria teddy", "victoria teddy1424", "teddy victoria", "teddy1424 victoria", "michael bubba", "michael bubbaplayspiano", "michael taryn", "bubba michael", "bubbaplayspinao michael", "taryn michael", "paunda anya", "anya paunda", "ben anya", "anya ben"];
                 if(matches.indexOf((thing1.toLowerCase().toString() + " " + thing2.toLowerCase().toString())) != -1){
                     return message.channel.sendEmbed({
